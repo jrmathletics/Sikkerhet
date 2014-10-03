f24cbcd75aef80087105a6fbae5dd53b5932bbe0
Mathias Faanes Olsen <mathias.faanes.olsen@gmail.com>
2014-10-03 15:29:43 +0200

    Lastet inn mappen
    Oblig1mappen ble lastet inn

---
+++ Sikkerhet/Controllers/UserController.cs
@@ -0,0 +1,138 @@
+﻿using Oblig1.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Oblig1.Controllers
+{
+    public class UserController : Controller
+    {
+        public ActionResult Index()
+        {
+            if (Session["LoggetInn"] == null)
+            {
+                Session["LoggetInn"] = false;
+                ViewBag.Innlogget = false;
+            }
+            else
+            {
+                ViewBag.Innlogget = (bool)Session["LoggetInn"];
+            }
+            
+            return View();
+        }
+        
+        [HttpPost]
+        public ActionResult Index(Oblig1.Models.User user)
+        {
+            if (Bruker_i_DB(user))
+            {
+                Session["LoggetInn"] = true;
+                ViewBag.Innlogget = true;
+                return View();
+            }
+            else
+            {
+                Session["LoggetInn"] = false;
+                ViewBag.Innlogget = false;
+                return View();
+            }
+            
+        }
+
+        public ActionResult Registrer()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Registrer(Oblig1.Models.User user)
+        {
+            Oblig1.Models.PastaContext db = new PastaContext();
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var newUser = new dbUser();
+                    byte[] passwordDb = createHash(user.Password);
+                    newUser.Password = passwordDb;
+                    user.Password = "11111111111";
+                    newUser.Email = user.Email;
+
+                    string innPostCode = user.city.Postcode;
+                    
+                    var foundCity = db.Cities.FirstOrDefault(p => p.Postcode == innPostCode);
+                    
+                    if (foundCity == null)
+                    {
+                        Debug.WriteLine("1");
+                        db.dbUsers.Add(newUser);
+                        Debug.WriteLine("2");
+                        db.Users.Add(user);
+                        db.SaveChanges();
+                        Debug.WriteLine("3");
+                        return RedirectToAction("Index");
+                    }
+                    else
+                    {
+                        user.city = foundCity;
+                        db.Users.Add(user);
+                        db.dbUsers.Add(newUser);
+                        db.SaveChanges();
+                        return RedirectToAction("Index");
+                    }
+                }
+                catch (Exception fail)
+                {
+
+                }
+
+
+            }
+
+            return View(user);
+        }
+        private static byte[] createHash(string inPassword)
+        {
+            byte[] inData, outData;
+            var algorithm = System.Security.Cryptography.SHA256.Create();
+            inData = System.Text.Encoding.ASCII.GetBytes(inPassword);
+            outData = algorithm.ComputeHash(inData);
+            return outData;
+        }
+
+        private static bool Bruker_i_DB(Oblig1.Models.User user)
+        {
+            using (var db = new PastaContext())
+            {
+                byte[] passordDb = createHash(user.Password);
+                dbUser foundUser = db.dbUsers.FirstOrDefault
+                    (b => b.Password == passordDb && b.Email == user.Email);
+                if (foundUser == null)
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+        }
+
+        public ActionResult InnLoggetSide()
+        {
+            if (Session["LoggetInn"] != null)
+            {
+                bool loggetInn = (bool)Session["LoggetInn"];
+                if(loggetInn)
+                {
+                    return View();
+                }
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Sikkerhet/Models/PastaModel.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+
+namespace Oblig1.Models
+{
+    public class User
+    {
+        [Required(ErrorMessage = "Firstname must be added")]
+        [StringLength(50, ErrorMessage = "Maximum 50 charactes in firstname")]
+        public string Firstname { get; set; }
+        [Required(ErrorMessage = "Surname must be added")]
+        [StringLength(50, ErrorMessage = "Maximum 50 characters in surname")]
+        public string Surname { get; set; }
+        [Required(ErrorMessage = "Address must be added")]
+        [StringLength(50, ErrorMessage = "Maximum 50 characters in address")]
+        public string Address { get; set; }
+        public string Postcode { get; set; }
+        [Key]
+        [Required(ErrorMessage = "E-mail must be added")]
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Phonenr must be added")]
+        [StringLength(20, ErrorMessage = "Maximum 20 characters in phonenr")]
+        public string Phonenr { get; set; }
+        [Required(ErrorMessage = "Password must be added")]
+        [StringLength(25, MinimumLength = 6, ErrorMessage = "Minimum 6 characters in password, maximum 25")]
+        public string Password { get; set; }
+        [Required]
+        public virtual City city { get; set; }
+        public virtual List<Order> Orders { get; set; }
+    }
+
+    public class dbUser
+    {
+        [Key]
+        public string Email { get; set; }
+        public byte[] Password { get; set; }
+    }
+
+    public class City
+    {
+        [Key]
+        [Required(ErrorMessage = "Postcode must be added")]
+        [StringLength(4, ErrorMessage = "Postcode must be 4 characters")]
+        public string Postcode { get; set; }
+        [Required(ErrorMessage = "City must be added")]
+        [StringLength(50, ErrorMessage = "Maximum 50 characters in city")]
+        public string Cityname { get; set; }
+        public virtual List<User> Users { get; set; }
+    }
+
+    public class Order
+    {
+        [Key]
+        public int Orderid { get; set; }
+        public virtual User user { get; set; }
+        //public virtual OrderItem orderitem { get; set; }
+        public virtual List<OrderLine> Orderlines { get; set; }
+    }
+
+    public class OrderLine
+    {
+        [Key]
+        public int Orderlineid { get; set; }
+        public virtual Order Order { get; set; }
+        public virtual Item Item { get; set; }
+        public int Itemamount { get; set; }
+    }
+
+    public class Item
+    {
+        [Key]
+        public int Itemid { get; set; }
+        public string Itemname { get; set; }
+        public int Itemprice { get; set; }
+        public int Currentstock { get; set; }
+        public string Itemtype { get; set; }
+        public virtual List<OrderLine> Orderlines { get; set; }
+    }
+
+    public class PastaContext : DbContext
+    {
+
+        public PastaContext()
+            : base("name=PastaModel")
+        {
+            Database.CreateIfNotExists();
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<City> Cities { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderLine> OrderLines { get; set; }
+        public DbSet<Item> Items { get; set; }
+        public DbSet<dbUser> dbUsers { get; set; }
+
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+
+    }
+
+}
